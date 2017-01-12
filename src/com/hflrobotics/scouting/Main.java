c076d06cb7a8d19bcba50e2677d5be6de69a4bc6
Michael <mguglielmo0@gmail.com>
2017-01-12 18:15:52 -0500

    Added dependencies for camera along with licenses

--- src/com/hflrobotics/scouting/Main.java
+++ src/com/hflrobotics/scouting/Main.java
@@ -1,12 +1,18 @@
 package com.hflrobotics.scouting;
 
+import com.github.sarxos.webcam.Webcam;
+
 public class Main
 {
-
 	
 	public static void main(String[] args)
 	{
-		
+		webcams();
 	}	
+	
+	private static void webcams()
+	{
+		System.out.println(Webcam.getWebcams());		
+	}
 	
 }
