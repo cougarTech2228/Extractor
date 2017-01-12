8cf0249ade9d4f2cd59d1c3da8a9f13630b80f26
Michael <mguglielmo0@gmail.com>
2017-01-12 17:36:06 -0500

    Added GUI and Main classes

---
+++ src/com/hflrobotics/scouting/GUI.java
@@ -0,0 +1,8 @@
+package com.hflrobotics.scouting;
+
+import javax.swing.JFrame;
+
+public class GUI extends JFrame
+{
+
+}
---
+++ src/com/hflrobotics/scouting/Main.java
@@ -0,0 +1,12 @@
+package com.hflrobotics.scouting;
+
+public class Main
+{
+
+	
+	public static void main(String[] args)
+	{
+		
+	}	
+	
+}
