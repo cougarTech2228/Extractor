bb71a49ebee80fa0501b3901344aaf06f94000b6
Michael <mguglielmo0@gmail.com>
2018-01-29 19:09:57 -0500

    Made individual data funcs v3 compliant!

--- src/com/hflrobotics/scouting/scraper/ScraperV3.java
+++ src/com/hflrobotics/scouting/scraper/ScraperV3.java
@@ -18,12 +18,15 @@ import com.google.gson.JsonParser;
 public class ScraperV3
 {
 
-	private static String VersionCode = "v1";
+	/**
+	 * Scraper for v3 of TBA API
+	 */
+	
 	private static final String TBA_URL = "https://www.thebluealliance.com/";
-	private static final String TBA_API_URL = TBA_URL + "api/v2/";
-	private static final String TBA_Header_NAME = "X-TBA-App-Id";
-	private static String TBA_Header_VALUE = "frc2228:Scouting:" + ScraperV3.VersionCode;
-
+	private static final String TBA_API_URL = TBA_URL + "api/v3/";
+	private static final String TBA_Header_NAME = "X-TBA-Auth-Key";
+	private static String TBA_Header_VALUE = ScraperAPIKey.getAPIKey();
+	
 	public static ArrayList<String[]> getCSVWriteableData(ArrayList<Team> teams)
 	{
 		ArrayList<String[]> result = new ArrayList<String[]>();
@@ -41,28 +44,33 @@ public class ScraperV3
 	 * Pulls the OPRs from The Blue Alliance for a given event and adds them to
 	 * the teams ArrayList
 	 * 
+	 * v3 Compliant.
+	 * 
 	 * @param eventCode
 	 *            - String yyyy[EVENT_CODE] from The Blue Alliance
 	 * @throws IOException 
 	 */
 	public static void getOPRs(String eventCode, ArrayList<Team> teams) throws IOException
 	{
-		String data = ScraperV3.getEventDataFromTBA(eventCode, "stats");
+		String data = ScraperV3.getEventDataFromTBA(eventCode, "oprs");
 		JsonParser parser = new JsonParser();
 		JsonObject obj = parser.parse(data).getAsJsonObject();
 
 		for (Map.Entry<String, JsonElement> entry : obj.get("oprs").getAsJsonObject().entrySet())
 		{
-			int i = ScraperV3.teamExists(entry.getKey(), teams);
+			String teamID = entry.getKey().substring(3); // Removes "frc" from team id (API: frc2228)
+			String teamOPR = entry.getValue().toString();
+			
+			int i = ScraperV3.teamExists(teamID, teams);
 			if (i != -1)
 			{
-				teams.get(i).opr = entry.getValue().toString();
+				teams.get(i).opr = teamOPR;
 			}
 			else
 			{
-				Team newTeam = new Team(entry.getKey());
+				Team newTeam = new Team(teamID);
 				teams.add(newTeam);
-				newTeam.opr = entry.getValue().toString();
+				newTeam.opr = teamOPR;
 			}
 		}
 	}
@@ -70,6 +78,8 @@ public class ScraperV3
 	/**
 	 * Pulls the rankings from The Blue Alliance for a given event and adds them
 	 * to the teams ArrayList
+	 * 
+	 * v3 Compliant
 	 * 
 	 * @param eventCode
 	 *            - String yyyy[EVENT_CODE] from The Blue Alliance
@@ -79,25 +89,25 @@ public class ScraperV3
 	{
 		String data = ScraperV3.getEventDataFromTBA(eventCode, "rankings");
 		JsonParser parser = new JsonParser();
-		JsonArray obj = parser.parse(data).getAsJsonArray();
-
+		JsonArray obj = parser.parse(data).getAsJsonObject().get("rankings").getAsJsonArray();
+		
 		for (JsonElement entry : obj)
 		{
-			JsonArray team = entry.getAsJsonArray();
-
-			if (!team.get(0).getAsString().equalsIgnoreCase("Rank"))
+			JsonObject entryObj = entry.getAsJsonObject();
+			String teamID = entryObj.get("team_key").getAsString().substring(3); // Removes "frc" from team id (API: frc2228)
+			String teamRank = entryObj.get("rank").getAsString();
+			
+			int i = ScraperV3.teamExists(teamID, teams);
+			
+			if (i != -1)
 			{
-				int i = ScraperV3.teamExists(team.get(1).getAsString(), teams);
-				if (i != -1)
-				{
-					teams.get(i).rank = team.get(0).getAsString();
-				}
-				else
-				{
-					Team newTeam = new Team(team.get(1).getAsString());
-					teams.add(newTeam);
-					newTeam.rank = team.get(0).getAsString();
-				}
+				teams.get(i).rank = teamRank;
+			}
+			else
+			{
+				Team newTeam = new Team(teamID);
+				teams.add(newTeam);
+				newTeam.rank = teamRank;
 			}
 		}
 	}
@@ -140,7 +150,8 @@ public class ScraperV3
 		URL url = new URL(ScraperV3.TBA_API_URL + "event/" + eventCode + "/" + request);
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 		con.addRequestProperty(ScraperV3.TBA_Header_NAME, TBA_Header_VALUE);
-
+		con.addRequestProperty("accept", "application/json");
+		
 		con.setRequestMethod("GET");
 		con.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
 		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
@@ -153,6 +164,5 @@ public class ScraperV3
 		}
 		in.close();
 		return response.toString();
-
 	}
 }
